ec8e9e6ca29cd964e2105b5abff9ef63a199fc12
Mohammad Reza Amini <mohammadreza.amini8294@gmail.com>
2024-10-17 18:56:08 +0330

    make some changes

--- app/Contracts/BaseRepositoryInterface.php
+++ app/Contracts/BaseRepositoryInterface.php
@@ -7,6 +7,7 @@ interface BaseRepositoryInterface
     public function getAll();
     public function storeData(array $resource);
     public function findById(int $id);
+    public function findBy(string $field,int|string $value);
     public function updateData(int $id,array $resource);
     public function deleteData(int $id);
 }
